fcbbe44f2d6a5129d33430164b0569572d2d9e12
richardardian22 <richardardian22@gmail.com>
2019-11-02 15:38:38 +0700

    laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Medikom Pustaka Mandiri</title>
+	</head>
+	<body>
+		<table border>
+		<caption align = top>DAFTAR MAHASISWA UNBO</caption>
+		<tr>
+			<th>No</th>
+			<th>Nama</th>
+			<th>Fakultas</th>
+			<th>Jurusan</th>
+			<th>Alamat</th>
+		</tr>
+		<tr>
+			<td>1.</td>
+			<td>Richard Ardian Yaputra</td>
+			<td>Ilmu Komputer</td>
+			<td>Sistem Informasi</td>
+			<td>Perumahan Taman Kota Baloi</td>
+		</tr>
+		<tr>
+			<td>2.</td>
+			<td>Richard Ardian Yaputra</td>
+			<td>Ilmu Komputer</td>
+			<td>Sistem Informasi</td>
+			<td>Perumahan Taman Kota Baloi</td>
+		</tr>
+	</body>
+</html>
+
+
